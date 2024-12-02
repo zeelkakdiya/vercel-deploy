f2c737d2b36c49cf10021b457f490f30351d8a3d
YashviKakadiya99 <yashvikakdiya99@gmail.com>
2024-12-02 10:12:36 -0800

    update the vercel setup commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 4000;
 
-app.get('/home', (req, res) => {
+app.get('/', (req, res) => {
   res.status(200).json('Welcome, your app is working well');
 });
 
