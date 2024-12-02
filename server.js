8817143bbfc339e8eef37f86ad0a1538df4d0456
YashviKakadiya99 <yashvikakdiya99@gmail.com>
2024-12-02 10:48:35 -0800

    RESOLVING ISSUE

--- server.js
+++
@@ -1,9 +0,0 @@
-
-const PORT = 4000;
-const app = require('./src/index')
-
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
-
-module.exports = app;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
+const PORT = 4000;
 
 app.get('/', (req, res) => {
   res.status(200).json('Welcome, your app is working well');
 });
 
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+})
 
 module.exports = app;
